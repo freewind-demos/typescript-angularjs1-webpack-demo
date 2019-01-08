d5d35e970674d10978088e73bda8041c8fe61bb8
freewind <nowindlee@gmail.com>
2019-01-08 21:51:33 +0800

    demo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import angular from 'angular';
+
 const app = angular.module('app', []);
 
 app.directive('myDirective', function () {
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: "development",
+    entry: './app.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: [{
+                loader: 'babel-loader', options: {
+                    presets: ['@babel/preset-env']
+                }
+            }]
+        }]
+    }
+}
